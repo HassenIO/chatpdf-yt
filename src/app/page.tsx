213eeabed4293d794922e2d0bcd0b6e7c8328b3c
Hassen Taidirt <hi@hassen.io>
2023-09-18 21:03:59 +0200

    Use first component using ShadCN UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <>
+      <h1>Hello world</h1>
+      <Button>Click me</Button>
+    </>
   )
 }
